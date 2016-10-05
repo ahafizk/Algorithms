facf0451a138855306645de1f3a69e55264955a7
Md Abdullah Al Hafiz Khan <mdkhan1@umbc.edu>
2016-10-05 11:14:09 -0400

    merge sort completed

--- Sorting/mergesort.cpp
+++ Sorting/mergesort.cpp
@@ -1,56 +1,70 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+/*
+ * to sort an array -> A, run the code $ merge_sort(A,0, A.length - 1)
+*/
+
+
 template<typename T>
 int merge(vector<T>&arr, int p, int q, int r)
 {
-    int n1 = q - p; //left array size
+    int n1 = q - p + 1; //left array size
     int n2 = r - q; //right array size
     vector<T>L,R;
-
-//    cout<<"Left array:: "<<endl;
-    for (int i=0;i<=n1;i++)
+    for (int i=0;i<n1;i++)
     {
         L.push_back(arr.at(p+i));
-//        cout << arr.at(p+i)<<" ";
+
     }
-//    cout<<endl;
     //copy the right subarray
-//    cout<<"Right array:: "<<endl;
     for(int j=0;j<n2;j++)
     {
-        R.push_back(arr.at(q+j));
-//        cout <<arr.at(q+j)+" ";
+        R.push_back(arr.at(q+j+1));
+    }
+
+    //get two iterator for two subarray
+    int i=0,j=0,k=0;
+    for (;k<r;k++)
+    {
+        //breaks the loop if any of the array (L or R) reached to its end
+        if (j>=n2 || i>=n1)break;
+
+        if (i<n1 && j<n2 && L.at(i)<=R.at(j))
+        {
+            arr.at(k) = L.at(i);
+            i++;
+        }
+        else if (j<n2)
+        {
+            arr.at(k) = R.at(j);
+            j++;
+        }
 
     }
-//    cout<<endl;
-    //get two iterator for two subarray
 
-    int i=0,j=0;
-    for (int k=0;k<r;k++)
+    //copy rest of L or R to arr
+    while(i<n1)
     {
-
-            if (i<=n1 && j<n2 && L.at(i)<=R.at(j))
-            {
-                arr.at(k) = L.at(i);
-                i++;
-            }
-            else if (j<n2)
-            {
-                arr.at(k) = R.at(j);
-                j++;
-            }
-    cout<<arr.at(k)<<" "<<endl;
+        arr.at(k)= L.at(i);
+        i++;
+        k++;
+    }
+    while(j<n2)
+    {
+       arr.at(k)= R.at(i);
+       i++;
+       k++;
     }
     return 0;
 }
+
 template<typename T>
 void merge_sort(vector<T> &arr, int p, int r)
 {
     if (p<r)
     {
         int q = (p+r)/2;
-//        cout<<q<<endl;
         merge_sort(arr,p,q);
         merge_sort(arr,q+1,r);
         merge(arr,p,q,r);
