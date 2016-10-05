405d47ba44a6c14063047055da63f05c16cfa207
Md Abdullah Al Hafiz Khan <mdkhan1@umbc.edu>
2016-10-05 16:53:20 -0400

    merge sort completed

--- Sorting/mergesort.cpp
+++ Sorting/mergesort.cpp
@@ -2,35 +2,50 @@
 #include<vector>
 using namespace std;
 /*
- * to sort an array -> A, run the code $ merge_sort(A,0, A.length - 1)
+ * to sort an array -> A, run the code $ merge_sort(A, 0, A.length - 1)
 */
-
+template<typename T>
+void print_arr(vector<T>A)
+{
+    int i=0;
+    for (;i<A.size();i++)
+        cout<<A.at(i)<<" ";
+    cout<<endl;
+}
 
 template<typename T>
-int merge(vector<T>&arr, int p, int q, int r)
+int merge(vector<T>&arr, int low, int mid, int high)
 {
-    int n1 = q - p + 1; //left array size
-    int n2 = r - q; //right array size
-    vector<T>L,R;
-    for (int i=0;i<n1;i++)
+
+    vector<T> L, R;
+
+    //copy the left sub array
+    for (int i=0; i<=mid; i++)
     {
-        L.push_back(arr.at(p+i));
+        L.push_back(arr.at(i));
 
     }
-    //copy the right subarray
-    for(int j=0;j<n2;j++)
+
+
+    //copy the right sub array
+    for(int j=mid+1; j<=high; j++)
     {
-        R.push_back(arr.at(q+j+1));
+        R.push_back(arr.at(j));
     }
 
-    //get two iterator for two subarray
-    int i=0,j=0,k=0;
-    for (;k<r;k++)
+
+
+    int i=0, j=0, k=0;
+    int n1 = L.size();
+    int n2 = R.size();
+
+    //compare and copy from left and right sub array to main array
+    for (; k<high; k++)
     {
         //breaks the loop if any of the array (L or R) reached to its end
-        if (j>=n2 || i>=n1)break;
+        if (i >= n1 || j >= n2) break;
 
-        if (i<n1 && j<n2 && L.at(i)<=R.at(j))
+        if (i < n1 && j<n2 && L.at(i)<=R.at(j))
         {
             arr.at(k) = L.at(i);
             i++;
@@ -43,30 +58,32 @@ int merge(vector<T>&arr, int p, int q, int r)
 
     }
 
-    //copy rest of L or R to arr
+    //copy rest of the left sub array to the main array
     while(i<n1)
     {
         arr.at(k)= L.at(i);
         i++;
         k++;
     }
+    //copy rest of the right sub array to the main array
     while(j<n2)
     {
-       arr.at(k)= R.at(i);
-       i++;
+       arr.at(k)= R.at(j);
+       j++;
        k++;
     }
+
     return 0;
 }
 
 template<typename T>
-void merge_sort(vector<T> &arr, int p, int r)
+void merge_sort(vector<T> &arr, int low, int high)
 {
-    if (p<r)
+    if (low < high)
     {
-        int q = (p+r)/2;
-        merge_sort(arr,p,q);
-        merge_sort(arr,q+1,r);
-        merge(arr,p,q,r);
+        int mid = (low + high)/2;
+        merge_sort(arr, low, mid);
+        merge_sort(arr,mid+1,high);
+        merge(arr,low,mid,high);
     }
 }
