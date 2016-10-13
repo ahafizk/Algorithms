64bf362a10375171f9c55fdee5b64c0ca855ab3b
Md Abdullah Al Hafiz Khan <mdkhan1@umbc.edu>
2016-10-12 23:54:30 -0400

    add searching algorithm

---
+++ Searching/binarysearch.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int binary_search(vector<int> &A, int key, int low, int high)
+{
+    if (low>high)
+    {
+        return -1;
+    }
+    int middle = (low+high)/2; //this is integer division, no need of floor() function
+
+    if (A.at(middle)==key)
+    {
+        return middle;
+    }
+    else if (A.at(middle)>key)
+    {
+        return binary_search(A,key,low, middle-1);
+    }
+    else if (A.at(middle)<key)
+    {
+        return binary_search(A,key,middle+1, high);
+    }
+}
